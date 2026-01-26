6e99015ef70f8efcc7de2ee2cccefb7182fee7de
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2026-01-26 20:55:48 -0300

    :construction: parra de navegação lateral e Header
    :construction: parra de navegação lateral e Header

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,24 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
-import { ListaDevedores } from './lista-devedores/lista-devedores'; 
 
 @Component({
-  selector: 'app-root',
-  standalone: true, 
-    imports: [ CommonModule, ListaDevedores ], 
+  selector: 'app-root',     // 🚨 ESSA LINHA DIZ: "Eu moro na tag <app-root>"
+  standalone: true,        // 🚨 ESSA LINHA DIZ: "Eu sou independente"
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+  ], 
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss'] // No Angular 17+ pode ser 'styleUrl' (singular), verifique qual você usa
 })
 export class AppComponent {
   title = 'frontend';
