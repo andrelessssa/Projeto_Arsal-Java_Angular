562881a87d9eb517026d4e6d5af26ae316b46433
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2026-01-27 11:09:34 -0300

    feat: implementada navegação completa (Início, Devedores, Relatórios, Login) com telaAtual
    feat: implementada navegação completa (Início, Devedores, Relatórios, Login) com telaAtual

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -5,11 +5,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ListaDevedores } from './lista-devedores/lista-devedores';
+import { Relatorio } from './relatorio/relatorio';
+import { Login } from './login/login';
 
 
 @Component({
-  selector: 'app-root',     // 🚨 ESSA LINHA DIZ: "Eu moro na tag <app-root>"
-  standalone: true,        // 🚨 ESSA LINHA DIZ: "Eu sou independente"
+  selector: 'app-root',
+  standalone: true,
   imports: [
     CommonModule,
     MatToolbarModule,
@@ -17,10 +20,34 @@ import { Component } from '@angular/core';
     MatButtonModule,
     MatIconModule,
     MatListModule,
+    ListaDevedores,
+    Relatorio,
+    Login
+
   ], 
   templateUrl: './app.html',
-  styleUrls: ['./app.scss'] // No Angular 17+ pode ser 'styleUrl' (singular), verifique qual você usa
+  styleUrls: ['./app.scss']
 })
 export class AppComponent {
   title = 'frontend';
+  telaAtual: string = 'inicio'; 
+
+  // 🚨 ADICIONE ESTA FUNÇÃO AQUI:
+  log(mensagem: string) {
+    console.log(mensagem);
+  }
+
+  mudarTela(nome: string) {
+    this.telaAtual = nome;
+    console.log('Tela atual agora é:', this.telaAtual);
+  }
+
+  voltarInicio() {
+    this.telaAtual = 'inicio';
+  }
+
+  mostrarLista() {
+    this.telaAtual = 'devedores';
+  }
+
 }
